9a24f539f4e2f2cbe15b587b221df0894c75a7c6
Burhan <buri.kapa@gmail.com>
2024-01-21 17:12:17 +0530

    Add project files.

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Baisc Ops
+
+            /*int burhan = Convert.ToInt32(Console.ReadLine()); //int variable
+
+            String imp = Console.ReadLine(); //String variable
+
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello Burhan!!!");
+
+            Console.WriteLine(burhan);
+
+            Console.WriteLine(imp); */
+
+            // Datatypes Examples
+
+            /*int a = 34;
+            float b = 34.4F;
+            double c = 34.44444D;
+            bool d = false;
+            char e = 'a';
+            string s = "Burhan";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(s);*/
+
+            // Type Casting
+
+            /*int a = (int)3.5;
+
+            Console.WriteLine(a);*/
+
+            // Inputs
+
+            // Console.Write("Enter Your Name: ");
+            // String x = Console.ReadLine();
+            // Console.WriteLine(x);
+
+            // Console.Write("Enter Your Age: ");
+            // int y = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(y);
+
+            // Operators In C#
+
+            /*int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());*/
+
+            // Arithmetic Oprators
+            /*Console.WriteLine(a + b);
+
+            Console.WriteLine(a-b);
+
+            Console.WriteLine(a*b);
+
+            Console.WriteLine(a/b);*/
+
+            // Assignment Operators
+
+            /*int a = 4;
+            int b = 5;
+
+            b += a;
+            b -= a;
+            b *= a;
+            b /= a;
+
+            Console.WriteLine(b);*/
+
+            // Logical Operators
+
+            /*and = &
+            or = ||
+            not = !*/
+
+            // Comparision Operator
+
+            /*int a =0 ; int b = 1;
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a == b);*/
+
+            // String Function
+
+            /*String a = "Hello \"";
+
+            Console.WriteLine(a.Length);
+            Console.WriteLine(a.ToUpper());
+            Console.WriteLine(a.ToLower());
+            Console.WriteLine(string.Concat(a, "World"));
+            Console.WriteLine(a.IndexOf('H'));
+            Console.WriteLine(a.Substring(2));*/
+
+            // Conditional Statements
+
+            /*int age = 56;
+
+            if (age > 18)
+            {
+                Console.WriteLine("You can Drive!");
+            }
+            else if(age > 18 && age > 50)
+            {
+                Console.WriteLine("You are above 50!");
+            }
+            else
+            { 
+                Console.WriteLine("You Cannot Drive!");
+            }*/
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
