66d9c1aea84060e58b057ba41433090e894006f1
Burhan <buri.kapa@gmail.com>
2024-01-22 10:30:13 +0530

    Added CLI based CalC

---
+++ Cal-C CLI App/Cal-C CLI App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cal_C_CLI_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            float num1 = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Enter Second Number: ");
+            float num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Select the Operator: ");
+            String ops = Console.ReadLine();
+
+            if (ops == "+")
+            {
+                Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            }
+            else if (ops == "-")
+            {
+                Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            }
+            else if (ops == "*")
+            {
+                Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+            }
+            else if (ops == "/")
+            {
+                Console.WriteLine(num1 + " / " + num2 + " = " + (float)(num1 / num2));
+            }
+            else if (ops == "^")
+            {
+                Console.WriteLine(num1 + " ^ " + num2 + " = " + (float)(Math.Pow(num1, num2)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
